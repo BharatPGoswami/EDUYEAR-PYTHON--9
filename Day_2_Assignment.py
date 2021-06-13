94998938b83c703d394da3b39847aea7567ccd57
BharatPGoswami <67272946+BharatPGoswami@users.noreply.github.com>
2021-06-13 14:49:03 +0530

    Day 2 Assignment solution
    Day 2 Assignment solution

---
+++ Day_2_Assignment.py
@@ -0,0 +1,33 @@
+##1. Take variables with values of different types
+
+str= "Hello World"
+
+print(str)
+
+Cr= 'a'
+
+print(Cr)
+
+data=2
+
+print(data)
+
+flt=2.5
+
+print(flt)
+
+
+
+
+##2. Print these in different lines and with appropriate messages (use .format())
+
+name = 'Bharat Puri Goswami'
+age =22
+desg= 'Software Engg'
+sal =6.58
+
+print("Hello I am {}. I'm {} year old.".format(name,age))
+print("I am working as a {}, with package of {}.".format(desg,sal))
+
+
+
